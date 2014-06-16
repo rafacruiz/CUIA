b153967c4937d4862bf6b98fb058614f0654bfc7
Fernando Llodra Belda <llodrab@gmail.com>
2014-06-16 18:37:56 +0200

    App for CUIA Subject
    Source code of ‘JobGlass’ an app for CUIA Subject

---
+++ JobGlass/src/com/example/jobglass/Maps.java
@@ -0,0 +1,13 @@
+package com.example.jobglass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Maps extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps);
+    }
+}
